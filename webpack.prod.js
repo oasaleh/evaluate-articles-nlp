0bbc9c360cc2d83a43838c2cb4d0948851a4c50c
Omar <56449814+oasaleh@users.noreply.github.com>
2021-10-31 23:17:32 -0500

    fix webpack production config file

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,10 @@ const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: './src/client/index.js',
+  output: {
+    libraryTarget: 'var',
+    library: 'Client',
+  },
   mode: 'production',
   module: {
     rules: [
